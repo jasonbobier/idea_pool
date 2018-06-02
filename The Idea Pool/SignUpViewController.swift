056a0f8574937dcca6cf1cd5efaafeda946cfdd4
Jason Bobier <jason@prismatix.com>
2018-06-02 08:57:22 +0200

    Removed localization for now.

---
+++ The Idea Pool/SignUpViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.swift
+//  The Idea Pool
+//
+//  Created by Jason Bobier on 6/1/18.
+//  Copyright © 2018 Jason Bobier. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+}
